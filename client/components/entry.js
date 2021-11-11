8d3b7047a6d87622b1940e99a4c7cabb637ba4da
unknown <kevintun01@gmail.com>
2021-11-11 12:39:19 +0900

    Posts can now read and rendered on the front end, including images. Still needs refactoring

--- client/components/entry.js
+++ client/components/entry.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'; 
 
 export const Entry = (props) => { //title, text, coordinates
+    const { selected } = props
+    console.log(selected)
     const [isEditable, setIsEditable] = useState(false)
     const [image, setImages] = useState([])
+
     
+    const renderPostContents = (contentsArray) => {
+        return contentsArray.map((item, idx) => {
+            if (item.slice(0,10) === 'data:image') {
+                return (<img key={idx} src={item}></img>)
+            } else {
+                return (<p key={idx}>{item}</p>)
+            } 
+        })
+    }
     
     return (
         <div
             onBlur={() => console.log('leaving textarea')}
         >
             <h2>{props.selected.title}</h2>
-            <p>{props.selected.text}</p>
+            {renderPostContents(selected.postContents)}
         </div>
     )
 }
